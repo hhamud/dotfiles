da9c3b1bb3b5bc08638ceeab82480d0de19e6bf7
Hamza <hamza.h.hamud@gmail.com>
2022-10-25 04:06:48 +0100

    fixing script

--- install_scripts/setup.sh
+++ install_scripts/setup.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env sh
 
 function install_emacs(){
-    echo "======> Installing emacs <======";	
-    sudo pacman -Syu emacs ripgrep;
-    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d;
-    rm -rf /home/$USER/.emacs.d/;
-    /home/$USER/.emacs.d/bin/doom install;
-    export PATH=$PATH:$HOME/.emacs.d/doom/bin;
-    doom doctor;
+    echo "======> Installing emacs <======"	
+    sudo pacman -Syu emacs ripgrep
+    rm -rf $HOME/.emacs.d/
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    $HOME/.emacs.d/bin/doom install
+    export PATH=$PATH:$HOME/.emacs.d/doom/bin
+    doom doctor
 }
 
 function check_init(){
     package=emacs
-    if pacman -Qs $package > /dev/null;
+    if pacman -Qs $package > /dev/null
     then
-        echo "The package $package is installed";
+        echo "The package $package is installed"
+        install_dotfiles
     else
-        install_emacs;
-        install_dotfiles;
+        install_emacs
+        install_dotfiles
 
     fi
 
 }
 
 function install_dotfiles(){
-    DIR="$HOME/.dotfiles"
-    if [ -d  "$DIR" ]; then
-        echo "dotfiles already exist"
-    else
-    	echo "======> installing dotfiles <======";
-        cd ~/.dotfiles;
+	echo "======> installing dotfiles <======"
+	cd ~/.dotfiles
 	# bash files
+	echo "======> symlinking bash files"
 	rm -rf $HOME/.bashrc $HOME/.bash_profile
 	ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 	ln -s $HOME/.dotfiles/.bash_profile  $HOME/.bash_profile
 
 	# Emacs config files
+	echo "======> symlinking emacs files"
 	rm -rf $HOME/.doom.d
-	ln -s $HOME/.doom.d  $HOME/.doom.d
+	ln -s $HOME/.dotfiles/.doom.d  $HOME/.doom.d
 
 	#vim config files
+	echo "======> symlinking vim files"
 	rm -rf $HOME/.config
-	ln -s $HOME/.config  $HOME/.config
-    fi
+	ln -s $HOME/.dotfiles/.config  $HOME/.config
+	
+	echo "======> setup complete <======"
 }
 
 
