169122a18aa71faa6b109802399cfcae001f6af9
Hamza <53880692+hhamud@users.noreply.github.com>
2024-01-04 19:47:37 +0000

    fix: org errors

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,41 +26,12 @@
 ;; they are implemented.
 
 
-;; dashboard configuation
-;;(use-package dashboard
-  ;;:ensure t
- ; :config
- ; (dashboard-setup-startup-hook))
 
-;;(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
 ;; doom themes
 (setq doom-theme 'doom-spacegrey)
 ; Set a custom font. Font choice can be important for performance.
 (setq doom-font (font-spec :family "JetBrains Mono" :size 15 :weight 'light))
-;; Set the title
-(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-;; Set the banner
-(setq dashboard-startup-banner "~/Pictures/network.png")
-;; Value can be
-;; 'official which displays the official emacs logo
-;; 'logo which displays an alternative emacs logo
-;; 1, 2 or 3 which displays one of the text banners
-;; "path/to/your/image.gif", "path/to/your/image.png" or "path/to/your/text.txt" which displays whatever gif/image/text you would prefer
-(setq dashboard-image-banner-max-width 300)
-;; Content is not centered by default. To center, set
-(setq dashboard-center-content t)
-
-
-
-(setq dashboard-items '((recents  . 5)
-                        (projects . 5)
-                        (agenda . 10)))
-
-;; icons for the dashboard
-(setq dashboard-set-heading-icons t)
-(setq dashboard-set-file-icons t)
-
 
 ;; find the correct monthly file review
 (after! org
@@ -120,13 +91,7 @@
 (setq org-outline-path-complete-in-steps nil)
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
-(setq org-archive-location "~/Documents/org/projects/archive/%s_archive::")
-
-(setq org-capture-templates '(
-  ("t" "todo" entry (file+headline "~/Documents/org/agenda.org" "Tasks:")
-    "** TODO %?\n   DEADLINE: <%<%Y-%m-%d %a>>\n"
-    :empty-lines 1)
-  ))
+(setq org-archive-location "~/Documents/org/agenda/projects/archive/%s_archive::")
 
 (setq org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
         '((sequence
@@ -141,29 +106,25 @@
 
 ;; set closed time on done tasks
 (setq org-log-done 'time)
-
-(setq org-agenda-files (directory-files-recursively "~/Documents/org/" "\\.org$"))
+(setq org-agenda-files (directory-files-recursively "~/Documents/org/agenda" "\\.org$"))
 (setq org-agenda-archives-mode t)
-;;(setq org-roam-directory "~/Documents/org/SlipBox")
-;;(setq org-roam-file-extensions '("org"))
+(setq org-roam-directory "~/Documents/org/info")
+(setq org-roam-file-extensions '("org"))
 
+(defun org-roam-backlink ()
+     "display the backlinks of the current org-roam buffer"
+     (interactive)
+     (progn
+        (display-buffer (get-buffer-create org-roam-buffer))
+        (org-roam-buffer-persistent-redisplay)))
 
-;;(defun org-roam-backlink ()
-     ;;"display the backlinks of the current org-roam buffer"
-     ;;(interactive)
-     ;;(progn
-        ;;(display-buffer (get-buffer-create org-roam-buffer))
-        ;;(org-roam-buffer-persistent-redisplay)))
-
-;;(add-hook 'md-roam-mode-hook 'org-roam-backlink)
+(add-hook 'md-roam-mode-hook 'org-roam-backlink)
 
 ;;source code block syntax highlighting
 (setq org-src-fontify-natively t
     org-src-tab-acts-natively t
     org-confirm-babel-evaluate nil
-    org-edit-src-content-indentation 0)
-)
-
+    org-edit-src-content-indentation 0))
 
 ;; doom-themes
 (after! doom-themes
@@ -173,43 +134,27 @@
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
-;;(use-package! mixed-pitch
-  ;;:hook (org-mode . variable-pitch-mode)
-  ;;:config
-  ;;(setq mixed-pitch-set-heigth t)
-  ;;;;(set-face-attribute 'variable-pitch nil :height 1.3)
-  ;;)
+(use-package! websocket
+    :after org-roam)
 
-;;(use-package! websocket
-    ;;:after org-roam)
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
 
-;;(use-package! org-roam-ui
-    ;;:after org-roam ;; or :after org
-;;;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;;;         a hookable mode anymore, you're advised to pick something yourself
-;;;;         if you don't care about startup time, use
-;;;;  :hook (after-init . org-roam-ui-mode)
-    ;;:config
-    ;;(setq org-roam-ui-sync-theme t
-          ;;org-roam-ui-follow t
-          ;;org-roam-ui-update-on-save t
-          ;;org-roam-ui-open-on-start t))
-;;
-;;
-;;(use-package! md-roam ; load immediately, before org-roam
-  ;;:config
-  ;;(setq md-roam-file-extension-single "md")
-  ;;(setq org-roam-file-extensions '("org" "md"))
-  ;;(setq md-roam-file-extension "md") ; default "md". Specify an extension such as "markdown"
-  ;;(md-roam-mode 1)) ; md-roam-mode must be active before org-roam-db-sync
-;;
-;;(org-roam-db-autosync-mode 1) ; autosync-mode triggers db-sync. md-roam-mode must be already active
+(org-roam-db-autosync-mode 1) ; autosync-mode triggers db-sync. md-roam-mode must be already active
 
 (defun markdown-string-block()
   "Creates yaml template for md-roam"
   (interactive)
   (insert (format "---\ntitle: \nid: %s\n---" (shell-command-to-string "uuidgen"))))
-
 
 (defun insert-file-name (file &optional relativep)
   "Read file name and insert it at point.
@@ -219,17 +164,11 @@
   (when relativep (setq file  (file-name-nondirectory file)))
   (format "%s" file))
 
-
-
 (defun insert-clipboard-image-to-buffer (title)
   "Finds the corresponding image from the file."
   (interactive "stitle of image: ")
   (shell-command "bash ~/Documents/Scripts/pasteImage.sh")
   (insert (concat "![ "(file-relative-name title) "](" (call-interactively 'insert-file-name) ")")))
-
-
-;;(define-key markdown-mode-map (kbd "C-c u") 'insert-clipboard-image-to-buffer)
-;;(define-key markdown-mode-map (kbd "C-c mm") 'markdown-string-block)
 
 ;; pareedit remaps
 (map!
@@ -248,13 +187,6 @@
     (set-face-attribute 'default nil :height 180)
     (set-face-attribute 'default nil :height 120))
 
-;; markdown-dnd-images -> allows dragging and dropping of images into markdown
-(require 'markdown-dnd-images)
-(setq dnd-save-directory "~/Documents/org/emacs_images")
-(setq dnd-save-buffer-name nil)
-(setq dnd-view-inline t)
-
-
 (setq org-table-eval-formulas t)
 (global-set-key (kbd "C-c i") 'org-edit-src-code)
 
@@ -262,7 +194,7 @@
 (defun copy-todo-to-file (todo-text)
   "Copy a completed todo to a file of your choosing."
   (interactive "sTodo text: ")
-  (let* ((dir "~/Documents/org/review/2023/")
+  (let* ((dir "~/Documents/org/agenda/review/2024/")
          (file (expand-file-name (read-file-name "Copy todo to file: " dir dir))))
     (find-file file)  ;; Open the file
     (goto-char (point-min))  ;; Go to the beginning of the file
@@ -482,4 +414,3 @@
   (set-fontset-font t ?• (font-spec :family "JetBrains Mono"))
   (set-fontset-font t ?⅓ (font-spec :family "Fira Code"))
   (set-fontset-font t ?⅔ (font-spec :family "Fira Code"))
-
